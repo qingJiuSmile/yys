03445493a0519c3adbaaca765021c2cbae7f22c6
tjy <tjy@weds.com.cn>
2019-12-06 16:43:28 +0800

    完善项目结构

---
+++ src/main/java/cn/qingjiu/yys/constant/CommConstant.java
@@ -0,0 +1,12 @@
+package cn.qingjiu.yys.constant;
+
+/*
+   常量
+ */
+public class CommConstant {
+
+    public final static Integer FAILED_FLAG = 601;
+    public final static Integer SUCCESSED_FLAG = 600;
+    public final static String SUCCESSED_MSG = "成功";
+    public final static String FAILED_MSG = "失败";
+}
--- src/main/java/cn/qingjiu/yys/service/UserService.java
+++ src/main/java/cn/qingjiu/yys/service/UserService.java
@@ -1,8 +1,14 @@
 package cn.qingjiu.yys.service;
 
+import cn.qingjiu.yys.entity.User;
 import cn.qingjiu.yys.mapper.UserMapper;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
 
 /** 
 * @Description: 用户信息业务类
@@ -16,5 +22,9 @@ public class UserService {
     @Autowired
     private UserMapper userMapper;
 
-
+    @PostMapping("/addUser")
+    @ApiOperation(value = "添加用户信息")
+    public List<User> addUser(@RequestBody User user){
+        return null;
+    }
 }
